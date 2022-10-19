6b687860419085577f996c87aaedff7ea2565dcd
Tomas Margio <tomasmr99@gmail.com>
2022-10-19 02:50:28 -0300

    Agrego libreria a utilizar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      <h1>Hola Matata</h1>
-      <h2> Hola mono cai</h2>
+      <div className='principal'>
+        <div className='secundario'>
+          <h1>Hola Matata</h1>
+          <p>Soy un parrafor y me gujsta asdasd</p>
+        </div>
+        <div className='secundario'>
+          <h1> Hola mono cai</h1>
+          <p>Lolazo queria usar el lorem</p>
+        </div>
+        <div className='botones'>
+          <button className='btn btn-success boton'>Comprar</button>
+          <button className='btn btn-danger boton'>Vender</button>
+          <button className='btn btn-outline-dark boton'>Borrar</button>
+        </div>
+      </div>
     </>
   );
 }
